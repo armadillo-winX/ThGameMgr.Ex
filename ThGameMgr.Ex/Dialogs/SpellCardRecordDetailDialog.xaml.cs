866f08d7731647db294f095e0ffc9715e0aa0faa
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-10-27 08:25:30 +0900

    SpellCardRecordDetailDialog実装

---
+++ ThGameMgr.Ex/Dialogs/SpellCardRecordDetailDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// SpellCardRecordDetailDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SpellCardRecordDetailDialog : Window
+    {
+        public SpellCardRecordDetailDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
