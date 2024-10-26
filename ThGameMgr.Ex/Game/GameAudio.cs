4e0925ceeb51be91ad99d5ebb88b1063d9d17099
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-10-26 09:46:11 +0900

    GameAudio 実装

---
+++ ThGameMgr.Ex/Game/GameAudio.cs
@@ -0,0 +1,56 @@
+﻿using NAudio.CoreAudioApi;
+
+namespace ThGameMgr.Ex.Game
+{
+    internal class GameAudio
+    {
+        public static float GetGameProcessAudioVolume(Process gameProcess)
+        {
+            MMDeviceEnumerator enumerator = new();
+            MMDevice defaultAudioDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+            if (defaultAudioDevice != null)
+            {
+                AudioSessionManager audioSessionManager = defaultAudioDevice.AudioSessionManager;
+                SessionCollection sessionsCollection = audioSessionManager.Sessions;
+
+                for (int i = 0; i < sessionsCollection.Count; i++)
+                {
+                    AudioSessionControl session = sessionsCollection[i];
+                    uint sessionId = session.GetProcessID;
+
+                    if (sessionId == gameProcess.Id)
+                        return session.SimpleAudioVolume.Volume;
+                }
+
+                return 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static void SetGameProcessAudioVolume(Process gameProcess, float volume)
+        {
+            MMDeviceEnumerator enumerator = new();
+            MMDevice defaultAudioDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+            if (defaultAudioDevice != null)
+            {
+                AudioSessionManager audioSessionManager = defaultAudioDevice.AudioSessionManager;
+                SessionCollection sessionsCollection = audioSessionManager.Sessions;
+
+                for (int i = 0; i < sessionsCollection.Count; i++)
+                {
+                    AudioSessionControl session = sessionsCollection[i];
+                    uint sessionId = session.GetProcessID;
+
+                    if (sessionId == gameProcess.Id)
+                    {
+                        session.SimpleAudioVolume.Volume = volume;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
