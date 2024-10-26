c825396531ce98ce272dc7ee3f906da87dec5ef0
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-10-27 07:58:20 +0900

    ScoreRecordDetaiDialog実装

---
+++ ThGameMgr.Ex/Dialogs/ScoreRecordDetailDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// ScoreRecordDetailDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ScoreRecordDetailDialog : Window
+    {
+        public ScoreRecordDetailDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
