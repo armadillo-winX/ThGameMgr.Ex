10896e2024a7bd2aefcfa7508adae86b2816ee3b
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-10-29 17:46:21 +0900

    不要なusingを削除

--- ThGameMgr.Ex/MainWindow.xaml.cs
+++ ThGameMgr.Ex/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ global using ThGameMgr.Ex.Score;
 global using ThGameMgr.Ex.Settings;
 
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
