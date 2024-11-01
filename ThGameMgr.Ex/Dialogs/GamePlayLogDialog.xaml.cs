4a3d11ba8469d8208572eb51aa4c11d15ad73b59
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-02 01:03:37 +0900

    GamePlayLogDialog実装

---
+++ ThGameMgr.Ex/Dialogs/GamePlayLogDialog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// GamePlayLogDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class GamePlayLogDialog : Window
+    {
+        public GamePlayLogDialog()
+        {
+            InitializeComponent();
+
+            try
+            {
+                ViewGamePlayLogData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"ゲーム実行履歴の取得に失敗しました。\n{ex.Message}",
+                    "エラー",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ViewGamePlayLogData()
+        {
+            string gamePlayLogRecordFile = $"{User.CurrentUserDirectoryPath}\\GamePlayLog.xml";
+            if (File.Exists(gamePlayLogRecordFile))
+            {
+                ObservableCollection<GamePlayLogData> gamePlayLogDataCollection = [];
+                gamePlayLogDataCollection = GamePlayLogRecorder.GetGamePlayLogDataCollection();
+                GameLogDataGrid.AutoGenerateColumns = false;
+                GameLogDataGrid.DataContext = gamePlayLogDataCollection;
+            }
+        }
+    }
+}
