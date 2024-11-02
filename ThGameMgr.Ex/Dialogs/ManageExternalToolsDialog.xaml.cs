e96a0d0e23c5f4aa370e8f9b001b69d933486a02
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-02 13:17:06 +0900

    ManageExternalToolsDialog実装

---
+++ ThGameMgr.Ex/Dialogs/ManageExternalToolsDialog.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Xml;
+
+namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// ManageExternalToolsDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ManageExternalToolsDialog : Window
+    {
+        public ManageExternalToolsDialog()
+        {
+            InitializeComponent();
+
+            if (!File.Exists($"{User.CurrentUserDirectoryPath}\\Settings\\ExternalTools.xml"))
+            {
+                try
+                {
+                    ExternalTool.CreateExternalConfigFile();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        $"外部ツールの一覧の取得に失敗しました。\n{ex.Message}",
+                        "エラー",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            try
+            {
+                GetExternalTools();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "エラー",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GetExternalTools()
+        {
+            ExternalToolsListBox.Items.Clear();
+
+            string exToolsConfig = $"{User.CurrentUserDirectoryPath}\\Settings\\ExternalTools.xml";
+            if (File.Exists(exToolsConfig))
+            {
+                XmlDocument exToolsConfigXml = new();
+                exToolsConfigXml.Load(exToolsConfig);
+                XmlNodeList exToolsNodeList = exToolsConfigXml.SelectNodes("ExternalTools/ExternalTool");
+                if (exToolsNodeList.Count > 0)
+                {
+                    foreach (XmlNode toolNode in exToolsNodeList)
+                    {
+                        string name = toolNode.SelectSingleNode("Name").InnerText;
+                        ExternalToolsListBox.Items.Add(name);
+                    }
+                }
+            }
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddExternalToolDialog addExternalToolDialog = new()
+                {
+                    Owner = this
+                };
+                if (addExternalToolDialog.ShowDialog() == true)
+                {
+                    GetExternalTools();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "エラー",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void DeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (ExternalToolsListBox.SelectedIndex > -1)
+            {
+                try
+                {
+                    string toolName = ExternalToolsListBox.SelectedItem.ToString();
+                    ExternalTool.DeleteExternalTool(toolName);
+                    GetExternalTools();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, ex.Message, "エラー",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
