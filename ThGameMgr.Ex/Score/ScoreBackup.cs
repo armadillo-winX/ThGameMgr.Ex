43ad490220b764d484726e63368c226796c06979
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-05 21:42:32 +0900

    ScoreBackup追加

---
+++ ThGameMgr.Ex/Score/ScoreBackup.cs
@@ -0,0 +1,50 @@
+﻿namespace ThGameMgr.Ex.Score
+{
+    internal class ScoreBackup
+    {
+        public static bool Create(string gameId)
+        {
+            string scoreFilePath = ScoreFile.GetScoreFilePath(gameId);
+            string backupDirectory = $"{User.CurrentUserDirectoryPath}\\backup\\{gameId}";
+
+            if (File.Exists(scoreFilePath))
+            {
+                if (!Directory.Exists(backupDirectory))
+                    Directory.CreateDirectory(backupDirectory);
+
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+
+                File.Copy(scoreFilePath, $"{backupDirectory}\\{timestamp}.bak", true);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Restore(string gameId, string backupFileName)
+        {
+            string scoreFilePath = ScoreFile.GetScoreFilePath(gameId);
+            string backupFilePath =
+                $"{User.CurrentUserDirectoryPath}\\backup\\{gameId}\\{backupFileName}";
+
+            File.Copy(backupFilePath, scoreFilePath, true);
+        }
+
+        public static string[] GetScoreBackupFiles(string gameId)
+        {
+            string backupDirectory = $"{User.CurrentUserDirectoryPath} \\backup\\{gameId}";
+            string[] scoreBackupFiles = Directory.GetFiles(backupDirectory, "*.bak", SearchOption.TopDirectoryOnly);
+
+            return scoreBackupFiles;
+        }
+
+        public static void Delete(string gameId, string backupFileName)
+        {
+            string backupFilePath = $"{User.CurrentUserDirectoryPath} \\backup\\{gameId}\\{backupFileName}";
+            File.Delete(backupFilePath);
+        }
+    }
+}
