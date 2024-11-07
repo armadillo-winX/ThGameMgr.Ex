61b046f3e76ccf0c03723eb1f2defe7a699df17d
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-07 18:44:23 +0900

    TextViewer実装

---
+++ ThGameMgr.Ex/TextViewer.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace ThGameMgr.Ex
+{
+    /// <summary>
+    /// TextViewer.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TextViewer : Window
+    {
+        public TextViewer()
+        {
+            InitializeComponent();
+        }
+
+        public string? FilePath { get; set; }
+
+        public string? Encode { get; set; }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(this.FilePath))
+            {
+                try
+                {
+                    //Shift_JISに対応させる
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                    StreamReader streamReader = new(this.FilePath, Encoding.GetEncoding(this.Encode));
+                    string text = streamReader.ReadToEnd();
+                    streamReader.Close();
+
+                    MainTextBox.Text = text;
+                }
+                catch (Exception ex)
+                {
+                    MainTextBox.Text =
+                        $"指定されたファイルの読み込みに失敗しました。\n{ex.Message}";
+                    this.Title = "エラー";
+                }
+            }
+            else
+            {
+                MainTextBox.Text = "指定されたファイルが存在しません。";
+            }
+        }
+    }
+}
