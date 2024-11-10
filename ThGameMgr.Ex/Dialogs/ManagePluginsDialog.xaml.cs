4562f45aba47c5a10b42ce79b20b8dc2f6c7eb27
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-10 23:43:06 +0900

    ManagePluginsDialog実装

---
+++ ThGameMgr.Ex/Dialogs/ManagePluginsDialog.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// ManagePluginsDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ManagePluginsDialog : Window
+    {
+        public ManagePluginsDialog()
+        {
+            InitializeComponent();
+
+            PluginDataGrid.AutoGenerateColumns = false;
+
+            ObservableCollection<PluginInfo> pluginInfoItems = [];
+
+            if (PluginHandler.GameFilesPlugins != null && PluginHandler.GameFilesPlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.GameFilesPlugins)
+                {
+                    pluginInfoItems.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.SelectedGamePlugins != null && PluginHandler.SelectedGamePlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.SelectedGamePlugins)
+                {
+                    pluginInfoItems.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.ToolPlugins != null && PluginHandler.ToolPlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.ToolPlugins)
+                {
+                    pluginInfoItems.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (pluginInfoItems.Count > 0)
+                PluginDataGrid.DataContext = pluginInfoItems;
+        }
+
+        private void PluginDataGridSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (PluginDataGrid.Items != null &&
+                PluginDataGrid.Items.Count > 0 &&
+                PluginDataGrid.SelectedIndex > -1 &&
+                PluginDataGrid.SelectedIndex < PluginDataGrid.Items.Count)
+            {
+                PluginInfo pluginInfo = PluginDataGrid.SelectedItem as PluginInfo;
+                if (pluginInfo != null)
+                    DetailBox.Text
+                        = @$"{pluginInfo.Name}
+Version.{pluginInfo.Version}
+開発者:{pluginInfo.Developer}
+
+[説明]
+{pluginInfo.Description}";
+            }
+        }
+    }
+}
