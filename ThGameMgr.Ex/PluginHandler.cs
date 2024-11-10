2d34d9918a9a94aee7a3dea944b7fb05f3b7b35f
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-10 20:38:18 +0900

    PluginHandler実装

---
+++ ThGameMgr.Ex/PluginHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace ThGameMgr.Ex
+{
+    internal class PluginHandler
+    {
+        public static List<dynamic>? GameFilesPlugins { get; set; }
+
+        public static void GetPlugin()
+        {
+            GameFilesPlugins = [];
+
+            string[] pluginFiles = Directory.GetFiles(
+                $"{PathInfo.AppLocation}\\Plugin", "*.dll", SearchOption.AllDirectories);
+
+            foreach (string pluginFile in pluginFiles)
+            {
+                try
+                {
+                    Assembly? dllAssembly = Assembly.LoadFrom(pluginFile);
+
+                    if (dllAssembly != null)
+                    {
+                        ClassifyPlugin(dllAssembly);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void ClassifyPlugin(Assembly dllAssembly)
+        {
+            //dllAssemblyの中のTypeをすべて取得し、プラグインのタイプがあるかチェックする
+            foreach (Type type in dllAssembly.GetTypes())
+            {
+                //対象のクラスのインスタンスを作成
+                dynamic plugin = Activator.CreateInstance(type);
+
+                if (type.BaseType == typeof(GameFilesPluginBase))
+                {
+                    GameFilesPlugins.Add(plugin);
+                }
+            }
+        }
+    }
+}
