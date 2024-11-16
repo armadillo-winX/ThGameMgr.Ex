ad1569dd68fe51a3c3a44f43e7b11b0c3cd44568
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-17 00:05:13 +0900

    ScoreRecordList追加

---
+++ ThGameMgr.Ex.Data/ScoreRecordList.cs
@@ -0,0 +1,21 @@
+﻿namespace ThGameMgr.Ex.Data
+{
+    public class ScoreRecordList
+    {
+        public string? Score { get; set; }
+
+        public string? Player { get; set; }
+
+        public string? Level { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Progress { get; set; }
+
+        public string? Date { get; set; }
+
+        public string? SlowRate { get; set; }
+
+        public string? OtherData { get; set; }
+    }
+}
