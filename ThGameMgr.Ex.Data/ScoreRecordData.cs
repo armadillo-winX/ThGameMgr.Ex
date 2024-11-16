f6243c27ed1943716b86b62645934d1828b4da3c
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-17 00:31:13 +0900

    ScoreRecordData: document comment追加

--- ThGameMgr.Ex.Data/ScoreRecordData.cs
+++ ThGameMgr.Ex.Data/ScoreRecordData.cs
@@ -2,20 +2,44 @@
 {
     public class ScoreRecordData
     {
+        /// <summary>
+        /// スコア値
+        /// </summary>
         public string? Score { get; set; }
 
+        /// <summary>
+        /// 自機名
+        /// </summary>
         public string? Player { get; set; }
 
+        /// <summary>
+        /// 難易度
+        /// </summary>
         public string? Level { get; set; }
 
+        /// <summary>
+        /// 名前
+        /// </summary>
         public string? Name { get; set; }
 
+        /// <summary>
+        /// 到達面
+        /// </summary>
         public string? Progress { get; set; }
 
+        /// <summary>
+        /// 日時
+        /// </summary>
         public string? Date { get; set; }
 
+        /// <summary>
+        /// 処理落ち率
+        /// </summary>
         public string? SlowRate { get; set; }
 
+        /// <summary>
+        /// その他のデータ
+        /// </summary>
         public string? OtherData { get; set; }
     }
 }
