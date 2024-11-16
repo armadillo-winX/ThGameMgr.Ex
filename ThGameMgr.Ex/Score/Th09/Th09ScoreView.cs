1d815183267cdb64390a300ad456144560c2b2fd
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-16 10:16:09 +0900

    GetScoreData追加

--- ThGameMgr.Ex/Score/Th09/Th09ScoreView.cs
+++ ThGameMgr.Ex/Score/Th09/Th09ScoreView.cs
@@ -9,5 +9,52 @@ namespace ThGameMgr.Ex.Score.Th09
     internal class Th09ScoreView
     {
         public static string[] _th09PlayersList = GamePlayers.GetGamePlayers(GameIndex.Th09).Split(',');
+    
+        public static void GetScoreData(bool displayUnchallengedCard)
+        {
+            string? gamePath = GameFile.GetGameFilePath(GameIndex.Th09);
+            string? scorePath = ScoreFile.GetScoreFilePath(GameIndex.Th09);
+
+            if (File.Exists(gamePath) && File.Exists(scorePath))
+            {
+                MemoryStream decodedData = new();
+                bool decodedResult = ScoreDecoder.Decode(GameIndex.Th09, scorePath, decodedData);
+                if (decodedResult)
+                {
+                    decodedData.Seek(0, SeekOrigin.Begin);
+                    using (decodedData)
+                    {
+                        byte[] bytes = new byte[decodedData.Length];
+
+                        _ = decodedData.Read(bytes, 0, (int)decodedData.Length);
+
+                        int i = 36;
+                        while (i < decodedData.Length)
+                        {
+                            int n = i + 4;
+                            int p = n + 2;
+                            //レコードのサイズデータを取得
+                            byte[] sizeData = bytes[n..p];
+                            int size = BitConverter.ToInt16(sizeData, 0);
+
+                            int r = i + size;
+                            byte[] typeData = bytes[i..n];
+                            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                            string type = Encoding.GetEncoding("Shift_JIS").GetString(typeData);
+                            if (type == "HSCR")
+                            {
+                                byte[] highscoreData = bytes[i..r];
+
+                                i += size;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
