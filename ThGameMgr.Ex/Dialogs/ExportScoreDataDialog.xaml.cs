49fe28825d4f76670b0e4b8aa12bf2ad2f53c94f
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-19 23:44:00 +0900

    ExportScoreDataDialog追加

---
+++ ThGameMgr.Ex/Dialogs/ExportScoreDataDialog.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+
+namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// ExportScoreDataDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ExportScoreDataDialog : Window
+    {
+        private string? GameId { get; set; }
+
+        public ExportScoreDataDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ExportButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.GameId))
+            {
+                SaveFileDialog saveFileDialog = new()
+                {
+                    FileName = $"{GameIndex.GetGameName(this.GameId)}スコアデータ.txt",
+                    Filter = "テキストファイル|*.txt|すべてのファイル|*.*"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string outputPath = saveFileDialog.FileName;
+                    try
+                    {
+                        ScoreData.ExportToTextFile(outputPath);
+                        MessageBox.Show(this, $"エクスポートしました。", "スコアデータをエクスポート",
+                            MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(this, $"エクスポートに失敗しました。\n{ex.Message}", "エラー",
+                            MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(this, "出力するゲームの種類が不明です。", "スコアデータのエクスポート",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+    }
+}
