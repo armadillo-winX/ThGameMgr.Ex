b7627ebcb31047171375c3c3d3591e51a44b5df0
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-23 09:16:28 +0900

    RenameReplayFileDialog実装

---
+++ ThGameMgr.Ex/Dialogs/RenameReplayFileDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// RenameReplayFileDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RenameReplayFileDialog : Window
+    {
+        private string? _replayFileName;
+
+        public string? ReplayFileName
+        {
+            get
+            {
+                return _replayFileName;
+            }
+
+            set
+            {
+                _replayFileName = value;
+                FileNameBox.Text = value;
+            }
+        }
+
+        public RenameReplayFileDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            _ = FileNameBox.Focus();
+        }
+
+        private void RenameButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (FileNameBox.Text.Length > 0)
+            {
+                this.ReplayFileName = FileNameBox.Text;
+                this.DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show(this, "ファイル名を空にはできません。", "リプレイファイルのリネーム",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+    }
+}
