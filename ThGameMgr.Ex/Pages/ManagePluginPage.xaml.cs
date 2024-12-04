2048ebdda5922edb926681ca22d9acb8fe7028c7
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-12-05 00:05:30 +0900

    ManagePluginPage追加

---
+++ ThGameMgr.Ex/Pages/ManagePluginPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System.Windows.Controls;
+
+namespace ThGameMgr.Ex.Pages
+{
+    /// <summary>
+    /// ManagePluginPage.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ManagePluginPage : Page
+    {
+        public ManagePluginPage()
+        {
+            InitializeComponent();
+
+            PluginDataGrid.AutoGenerateColumns = false;
+
+            if (PluginHandler.StartGamePlugins != null && PluginHandler.StartGamePlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.StartGamePlugins)
+                {
+                    PluginDataGrid.Items.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.GameFilesPlugins != null && PluginHandler.GameFilesPlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.GameFilesPlugins)
+                {
+                    PluginDataGrid.Items.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.SelectedGamePlugins != null && PluginHandler.SelectedGamePlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.SelectedGamePlugins)
+                {
+                    PluginDataGrid.Items.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.ScoreRecordsPlugins != null && PluginHandler.ScoreRecordsPlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.ScoreRecordsPlugins)
+                {
+                    PluginDataGrid.Items.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.SpellCardRecordsPlugins != null && PluginHandler.SpellCardRecordsPlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.SpellCardRecordsPlugins)
+                {
+                    PluginDataGrid.Items.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.AllScoreRecordsPlugins != null && PluginHandler.AllScoreRecordsPlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.AllScoreRecordsPlugins)
+                {
+                    PluginDataGrid.Items.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+
+            if (PluginHandler.ToolPlugins != null && PluginHandler.ToolPlugins.Count > 0)
+            {
+                foreach (dynamic plugin in PluginHandler.ToolPlugins)
+                {
+                    PluginDataGrid.Items.Add(new PluginInfo()
+                    {
+                        Name = plugin.Name,
+                        Version = plugin.Version,
+                        Developer = plugin.Developer,
+                        Description = plugin.Description
+                    });
+                }
+            }
+        }
+
+        private void PluginDataGridSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (PluginDataGrid.Items != null &&
+                PluginDataGrid.Items.Count > 0 &&
+                PluginDataGrid.SelectedIndex > -1)
+            {
+                PluginInfo pluginInfo = PluginDataGrid.SelectedItem as PluginInfo;
+                if (pluginInfo != null)
+                    DetailBox.Text
+                        = @$"{pluginInfo.Name}
+Version.{pluginInfo.Version}
+開発者:{pluginInfo.Developer}
+
+[説明]
+{pluginInfo.Description}";
+            }
+        }
+    }
+}
