e10aa90e99776c29e4698a5df29a99bd30519000
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-12-07 02:14:00 +0900

    Th16: Levelデータ取得実装

--- ThGameMgr.Ex/Score/Th16/Th16ScoreData.cs
+++ ThGameMgr.Ex/Score/Th16/Th16ScoreData.cs
@@ -151,16 +151,20 @@ namespace ThGameMgr.Ex.Score.Th16
             int practiceGet = BitConverter.ToInt32(PRACTICE_GET_DATA, 0);
             int practiceChallenge = BitConverter.ToInt32(PRACTICE_CHALLENGE_DATA, 0);
 
+            string level = SpellCard.ParseLevelData(LEVEL_DATA);
+
             SpellCardRecordData spellCardRecordList = new()
             {
                 TryCount = challenge.ToString(),
-                GetCount = get.ToString()
+                GetCount = get.ToString(),
+                Level = level
             };
 
             SpellCardRecordData practiceSpellCardRecordList = new()
             {
                 TryCount = practiceChallenge.ToString(),
-                GetCount = practiceGet.ToString()
+                GetCount = practiceGet.ToString(),
+                Level = level
             };
 
             ObservableCollection<SpellCardRecordData> spellCardRecordLists = [spellCardRecordList, practiceSpellCardRecordList];
@@ -228,6 +232,7 @@ namespace ThGameMgr.Ex.Score.Th16
                 TryCount = allChallengeCount.ToString(),
                 GetCount = allGetCount.ToString(),
                 Rate = allGetRate,
+                Level = cardDataReimu[0].Level,
                 Enemy = spellcardData.Enemy,
                 Place = spellcardData.Place
             };
@@ -239,6 +244,7 @@ namespace ThGameMgr.Ex.Score.Th16
                 TryCount = allPracticeChallengeCount.ToString(),
                 GetCount = allPracticeGetCount.ToString(),
                 Rate = allPracticeGetRate,
+                Level = cardDataReimu[1].Level,
                 Enemy = spellcardData.Enemy,
                 Place = spellcardData.Place
             };
