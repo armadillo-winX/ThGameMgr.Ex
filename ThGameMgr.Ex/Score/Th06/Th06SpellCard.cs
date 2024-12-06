1fb13616829e35586863af37433619f51085d20f
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-12-07 03:02:27 +0900

    全場所データの簡素化

--- ThGameMgr.Ex/Score/Th06/Th06SpellCard.cs
+++ ThGameMgr.Ex/Score/Th06/Th06SpellCard.cs
@@ -11,448 +11,448 @@ namespace ThGameMgr.Ex.Score.Th06
                 CardID = "1",
                 CardName = "月符「ムーンライトレイ」",
                 Enemy = "ルーミア",
-                Place = "1面道中"
+                Place = "1面"
             },
             new SpellCard()
             {
                 CardID = "2",
                 CardName = "夜符「ナイトバード」",
                 Enemy = "ルーミア",
-                Place = "1面ボス"
+                Place = "1面"
             },
             new SpellCard()
             {
                 CardID = "3",
                 CardName = "闇符「ディマーケイション」",
                 Enemy = "ルーミア",
-                Place = "1面ボス"
+                Place = "1面"
             },
             new SpellCard()
             {
                 CardID = "4",
                 CardName = "氷符「アイシクルフォール」",
                 Enemy = "チルノ",
-                Place = "2面ボス"
+                Place = "2面"
             },
             new SpellCard()
             {
                 CardID = "5",
                 CardName = "雹符「ヘイルストーム」",
                 Enemy = "チルノ",
-                Place = "2面ボス"
+                Place = "2面"
             },
             new SpellCard()
             {
                 CardID = "6",
                 CardName = "凍符「パーフェクトフリーズ」",
                 Enemy = "チルノ",
-                Place = "2面ボス"
+                Place = "2面"
             },
             new SpellCard()
             {
                 CardID = "7",
                 CardName = "雪符「ダイアモンドブリザード」",
                 Enemy = "チルノ",
-                Place = "2面ボス"
+                Place = "2面"
             },
             new SpellCard()
             {
                 CardID = "8",
                 CardName = "華符「芳華絢爛」",
                 Enemy = "紅美鈴",
-                Place = "3面道中"
+                Place = "3面"
             },
             new SpellCard()
             {
                 CardID = "9",
                 CardName = "華符「セラギネラ９」",
                 Enemy = "紅美鈴",
-                Place = "3面道中"
+                Place = "3面"
             },
             new SpellCard()
             {
                 CardID = "10",
                 CardName = "虹符「彩虹の風鈴」",
                 Enemy = "紅美鈴",
-                Place = "3面ボス"
+                Place = "3面"
             },
             new SpellCard()
             {
                 CardID = "11",
                 CardName = "幻符「華想夢葛」",
                 Enemy = "紅美鈴",
-                Place = "3面ボス"
+                Place = "3面"
             },
             new SpellCard()
             {
                 CardID = "12",
                 CardName = "彩符「彩雨」",
                 Enemy = "紅美鈴",
-                Place = "3面ボス"
+                Place = "3面"
             },
             new SpellCard()
             {
                 CardID = "13",
                 CardName = "彩符「彩光乱舞」",
                 Enemy = "紅美鈴",
-                Place = "3面ボス"
+                Place = "3面"
             },
             new SpellCard()
             {
                 CardID = "14",
                 CardName = "彩符「極彩颱風」",
                 Enemy = "紅美鈴",
-                Place = "3面ボス"
+                Place = "3面"
             },
             new SpellCard()
             {
                 CardID = "15",
                 CardName = "火符「アグニシャイン」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "16",
                 CardName = "水符「プリンセスウンディネ」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "17",
                 CardName = "木符「シルフィホルン」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "18",
                 CardName = "土符「レイジィトリリトン」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "19",
                 CardName = "金符「メタルファティーグ」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "20",
                 CardName = "火符「アグニシャイン上級」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "21",
                 CardName = "木符「シルフィホルン上級」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "22",
                 CardName = "土符「レイジィトリリトン上級」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "23",
                 CardName = "火符「アグニレイディアンス」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "24",
                 CardName = "水符「ベリーインレイク」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "25",
                 CardName = "木符「グリーンストーム」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "26",
                 CardName = "土符「トリリトンシェイク」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "27",
                 CardName = "金符「シルバードラゴン」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "28",
                 CardName = "火＆土符「ラーヴァクロムレク」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "29",
                 CardName = "木＆火符「フォレストブレイズ」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "30",
                 CardName = "水＆木符「ウォーターエルフ」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "31",
                 CardName = "金＆水符「マーキュリポイズン」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "32",
                 CardName = "土＆金符「エメラルドメガリス」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "4面ボス"
+                Place = "4面"
             },
             new SpellCard()
             {
                 CardID = "33",
                 CardName = "奇術「ミスディレクション」",
                 Enemy = "十六夜咲夜",
-                Place = "5面道中"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "34",
                 CardName = "奇術「幻惑ミスディレクション」",
                 Enemy = "十六夜咲夜",
-                Place = "5面道中"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "35",
                 CardName = "幻在「クロックコープス」",
                 Enemy = "十六夜咲夜",
-                Place = "5面ボス"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "36",
                 CardName = "幻象「ルナクロック」",
                 Enemy = "十六夜咲夜",
-                Place = "5面ボス"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "37",
                 CardName = "メイド秘技「操りドール」",
                 Enemy = "十六夜咲夜",
-                Place = "5面ボス"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "38",
                 CardName = "幻幽「ジャック・ザ・ルドビレ」",
                 Enemy = "十六夜咲夜",
-                Place = "5面ボス"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "39",
                 CardName = "幻世「ザ・ワールド」",
                 Enemy = "十六夜咲夜",
-                Place = "5面ボス"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "40",
                 CardName = "メイド秘技「殺人ドール」",
                 Enemy = "十六夜咲夜",
-                Place = "5面ボス"
+                Place = "5面"
             },
             new SpellCard()
             {
                 CardID = "41",
                 CardName = "奇術「エターナルミーク」",
                 Enemy = "十六夜咲夜",
-                Place = "6面道中"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "42",
                 CardName = "天罰「スターオブダビデ」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "43",
                 CardName = "冥符「紅色の冥界」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "44",
                 CardName = "呪詛「ブラド・ツェペシュの呪い」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "45",
                 CardName = "紅符「スカーレットシュート」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "46",
                 CardName = "「レッドマジック」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "47",
                 CardName = "神罰「幼きデーモンロード」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "48",
                 CardName = "獄符「千本の針の山」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "49",
                 CardName = "神術「吸血鬼幻想」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "50",
                 CardName = "紅符「スカーレットマイスタ」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "51",
                 CardName = "「紅色の幻想郷」",
                 Enemy = "レミリア・スカーレット",
-                Place = "6面ボス"
+                Place = "6面"
             },
             new SpellCard()
             {
                 CardID = "52",
                 CardName = "月符「サイレントセレナ」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "Extra道中"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "53",
                 CardName = "日符「ロイヤルフレア」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "Extra道中"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "54",
                 CardName = "火水木金土符「賢者の石」",
                 Enemy = "パチュリー・ノーレッジ",
-                Place = "Extra道中"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "55",
                 CardName = "禁忌「クランベリートラップ」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "56",
                 CardName = "禁忌「レーヴァテイン」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "57",
                 CardName = "禁忌「フォーオブアカインド」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "58",
                 CardName = "禁忌「カゴメカゴメ」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "59",
                 CardName = "禁忌「恋の迷路」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "60",
                 CardName = "禁弾「スターボウブレイク」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "61",
                 CardName = "禁弾「カタディオプトリック」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "62",
                 CardName = "禁弾「過去を刻む時計」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "63",
                 CardName = "秘弾「そして誰もいなくなるか？」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             },
             new SpellCard()
             {
                 CardID = "64",
                 CardName = "ＱＥＤ「４９５年の波紋」",
                 Enemy = "フランドール・スカーレット",
-                Place = "Extraボス"
+                Place = "Extra"
             }
         ];
 
