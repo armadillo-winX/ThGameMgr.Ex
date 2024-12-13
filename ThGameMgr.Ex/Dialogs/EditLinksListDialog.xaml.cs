261054b211f79690f05c150066c16900352a876c
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-12-14 02:22:07 +0900

    EditLinksListDialog機能実装

--- ThGameMgr.Ex/Dialogs/EditLinksListDialog.xaml.cs
+++ ThGameMgr.Ex/Dialogs/EditLinksListDialog.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Collections.Generic;
 
 namespace ThGameMgr.Ex.Dialogs
 {
@@ -22,6 +10,92 @@ namespace ThGameMgr.Ex.Dialogs
         public EditLinksListDialog()
         {
             InitializeComponent();
+
+            GetLinkLists();
+        }
+
+        private void GetLinkLists()
+        {
+            LinkListsDataGrid.Items.Clear();
+            try
+            {
+                string linksListFile = $"{User.CurrentUserDirectoryPath}\\links.txt";
+                if (!File.Exists(linksListFile))
+                    File.Copy(PathInfo.DefaultLinksListFile, linksListFile);
+
+                IEnumerable<string> lines = File.ReadLines(linksListFile);
+                foreach (string line in lines)
+                {
+                    string[] strings = line.Split('|');
+                    string name = strings[0];
+                    string url = strings[1];
+
+                    LinkListsDataGrid.Items.Add(new LinkData
+                    {
+                        Name = name,
+                        Url = url,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"リンクのリストの取得に失敗しました。\n{ex.Message}", "エラー",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            AddLinkDialog addLinkDialog = new()
+            {
+                Owner = this
+            };
+
+            if (addLinkDialog.ShowDialog() == true)
+            {
+                if (addLinkDialog.LinkData != null)
+                {
+                    LinkListsDataGrid.Items.Add(addLinkDialog.LinkData);
+                }
+            }
+        }
+
+        private void RemoveButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (LinkListsDataGrid.SelectedIndex > -1)
+            {
+                LinkListsDataGrid.Items.Remove(LinkListsDataGrid.SelectedItem);
+            }
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            string data = "";
+
+            try
+            {
+                string linksListFile = $"{User.CurrentUserDirectoryPath}\\links.txt";
+
+                if (LinkListsDataGrid.Items.Count > 0)
+                {
+                    for (int i = 0; i < LinkListsDataGrid.Items.Count; i++)
+                    {
+                        LinkData linkData = LinkListsDataGrid.Items[i] as LinkData;
+                        data += $"{linkData.Name}|{linkData.Url}\r\n";
+                    }
+                }
+
+                StreamWriter streamWriter = new(linksListFile, false);
+                streamWriter.Write(data);
+                streamWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, $"リンクリストの保存に失敗しました。\n{ex.Message}", "エラー",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
         }
     }
 }
