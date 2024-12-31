2dc31732b9260a5865743b715de911d73275ab34
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2025-01-01 02:00:58 +0900

    ClearRecordData追加

---
+++ ThGameMgr.Ex.Data/ClearRecordData.cs
@@ -0,0 +1,12 @@
+﻿namespace ThGameMgr.Ex.Data
+{
+    public class ClearRecordData
+    {
+        public string? Player { get; set; }
+        public string? Easy { get; set; }
+        public string? Normal { get; set; }
+        public string? Hard { get; set; }
+        public string? Lunatic { get; set; }
+        public string? Extra { get; set; }
+    }
+}
