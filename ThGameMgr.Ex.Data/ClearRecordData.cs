8a9df3f887044d6069c0d091300050ddc0715d82
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2025-01-01 02:03:09 +0900

    summaryの追加

--- ThGameMgr.Ex.Data/ClearRecordData.cs
+++ ThGameMgr.Ex.Data/ClearRecordData.cs
@@ -2,11 +2,34 @@
 {
     public class ClearRecordData
     {
+        /// <summary>
+        /// 自機名
+        /// </summary>
         public string? Player { get; set; }
+
+        /// <summary>
+        /// 難易度 Easy でのクリア達成度
+        /// </summary>
         public string? Easy { get; set; }
+
+        /// <summary>
+        /// 難易度 Normal でのクリア達成度
+        /// </summary>
         public string? Normal { get; set; }
+
+        /// <summary>
+        /// 難易度 Hard でのクリア達成度
+        /// </summary>
         public string? Hard { get; set; }
+
+        /// <summary>
+        /// 難易度 Lunatic でのクリア達成度
+        /// </summary>
         public string? Lunatic { get; set; }
+
+        /// <summary>
+        /// 難易度 Extra でのクリア達成度
+        /// </summary>
         public string? Extra { get; set; }
     }
 }
