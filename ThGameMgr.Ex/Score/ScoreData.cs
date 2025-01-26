c1f48cc8ba822d55288526ffdcaff7d1172177eb
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2025-01-26 23:33:02 +0900

    ScoreData.Get: 返り値を設定

--- ThGameMgr.Ex/Score/ScoreData.cs
+++ ThGameMgr.Ex/Score/ScoreData.cs
@@ -16,7 +16,7 @@ namespace ThGameMgr.Ex.Score
 
         public static ObservableCollection<SpellCardRecordData>? SpellPracticeRecordLists { get; set; }
 
-        public static void Get(string gameId)
+        public static bool Get(string gameId)
         {
             GameId = gameId;
 
@@ -38,54 +38,71 @@ namespace ThGameMgr.Ex.Score
             if (gameId == GameIndex.Th06)
             {
                 Th06.Th06ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th07)
             {
                 Th07.Th07ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th08)
             {
                 Th08.Th08ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th09)
             {
                 Th09.Th09ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th10)
             {
                 Th10.Th10ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th11)
             {
                 Th11.Th11ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th12)
             {
                 Th12.Th12ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th13)
             {
                 Th13.Th13ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th14)
             {
                 Th14.Th14ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th15)
             {
                 Th15.Th15ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th16)
             {
                 Th16.Th16ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th17)
             {
                 Th17.Th17ScoreData.Get();
+                return true;
             }
             else if (gameId == GameIndex.Th18)
             {
                 Th18.Th18ScoreData.Get();
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
