9c1cdb4b6ab7931e30663aa3de44151a9a9a4d20
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-16 13:42:04 +0900

    userDirectoryName が Null の時の処理を実装

--- ThGameMgr.Ex/User.cs
+++ ThGameMgr.Ex/User.cs
@@ -170,7 +170,10 @@ namespace ThGameMgr.Ex
 
         public static void Delete(string userName)
         {
-            string userDirectoryName = GetUserDirectoryName(userName);
+            string? userDirectoryName = GetUserDirectoryName(userName);
+            if (userDirectoryName == null)
+                throw new InvalidOperationException($"ユーザー '{userName}' は存在しないユーザーです．");
+
             string userDirectory = $"{_usersDirectory}\\{userDirectoryName}";
 
             string? usersIndexFile = PathInfo.UsersIndexFile;
