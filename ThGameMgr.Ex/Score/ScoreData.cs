b37212492da5c12d501b296fda38a18a5b72d42e
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-16 10:47:31 +0900

    Null チェックを導入

--- ThGameMgr.Ex/Score/ScoreData.cs
+++ ThGameMgr.Ex/Score/ScoreData.cs
@@ -283,7 +283,8 @@ namespace ThGameMgr.Ex.Score
                 data += $"御札戦歴\r\n-----------------------------------------------------\r\n";
                 foreach (SpellCardRecordData spellCardRecordData in SpellCardRecordLists)
                 {
-                    if (int.Parse(spellCardRecordData.TryCount) > 0)
+                    if (!string.IsNullOrEmpty(spellCardRecordData.TryCount) &&
+                        int.Parse(spellCardRecordData.TryCount) > 0)
                     {
                         data += $"No.{spellCardRecordData.CardID}\r\n{spellCardRecordData.CardName}\r\n取得数: {spellCardRecordData.GetCount}\r\n挑戦数: {spellCardRecordData.TryCount}\r\n取得率: {spellCardRecordData.Rate}\r\n発動場所: {spellCardRecordData.Place}\r\n術者: {spellCardRecordData.Enemy}\r\n\r\n";
                     }
