1b59d692f3227afc93c322c7ea16b625b3efecea
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-16 11:35:19 +0900

    SpellCardRecordData の各プロパティに required ステートメント追加

--- ThGameMgr.Ex.Data/SpellCardRecordData.cs
+++ ThGameMgr.Ex.Data/SpellCardRecordData.cs
@@ -5,41 +5,41 @@
         /// <summary>
         /// スペルカードの番号
         /// </summary>
-        public string? CardID { get; set; }
+        public required string CardID { get; set; }
 
         /// <summary>
         /// スペルカード名
         /// </summary>
-        public string? CardName { get; set; }
+        public required string CardName { get; set; }
 
         /// <summary>
         /// 挑戦回数
         /// </summary>
-        public string? TryCount { get; set; }
+        public required string TryCount { get; set; }
 
         /// <summary>
         /// 取得回数
         /// </summary>
-        public string? GetCount { get; set; }
+        public required string GetCount { get; set; }
 
         /// <summary>
         /// 取得率(百分率)
         /// </summary>
-        public string? Rate { get; set; }
+        public required string Rate { get; set; }
 
         /// <summary>
         /// スペルカードの出現難易度
         /// </summary>
-        public string? Level { get; set; }
+        public required string Level { get; set; }
 
         /// <summary>
         /// スペルカードの発動場所
         /// </summary>
-        public string? Place { get; set; }
+        public required string Place { get; set; }
 
         /// <summary>
         /// スペルカードの術者
         /// </summary>
-        public string? Enemy { get; set; }
+        public required string Enemy { get; set; }
     }
 }
