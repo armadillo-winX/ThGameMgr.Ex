2c75cb260ed3f0e6b6a6194a42c268dcffee91c0
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-22 07:03:21 +0900

    ScoreDataRetrieveTime への打刻を追加

--- ThGameMgr.Ex/Score/ScoreData.cs
+++ ThGameMgr.Ex/Score/ScoreData.cs
@@ -49,66 +49,79 @@ namespace ThGameMgr.Ex.Score
                 if (gameId == GameIndex.Th06)
                 {
                     Th06.Th06ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th07)
                 {
                     Th07.Th07ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th08)
                 {
                     Th08.Th08ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th09)
                 {
                     Th09.Th09ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th10)
                 {
                     Th10.Th10ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th11)
                 {
                     Th11.Th11ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th12)
                 {
                     Th12.Th12ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th13)
                 {
                     Th13.Th13ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th14)
                 {
                     Th14.Th14ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th15)
                 {
                     Th15.Th15ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th16)
                 {
                     Th16.Th16ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th17)
                 {
                     Th17.Th17ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else if (gameId == GameIndex.Th18)
                 {
                     Th18.Th18ScoreData.Get();
+                    ScoreDataRetrieveTime = DateTime.Now;
                     return true;
                 }
                 else
