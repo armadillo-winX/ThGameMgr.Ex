91f973cd25ce391debf71f59e18f405ea9acb6ec
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-22 03:39:56 +0900

    Null チェック導入

--- ThGameMgr.Ex/Game/GamePlayLogRecorder.cs
+++ ThGameMgr.Ex/Game/GamePlayLogRecorder.cs
@@ -60,13 +60,19 @@ namespace ThGameMgr.Ex.Game
             {
                 foreach (XmlNode gameLog in allGameLogs)
                 {
+                    XmlNode? gameIdNode = gameLog.SelectSingleNode("GameId");
+                    XmlNode? gameNameNode = gameLog.SelectSingleNode("GameName");
+                    XmlNode? gameStartTimeNode = gameLog.SelectSingleNode("GameStartTime");
+                    XmlNode? gameEndTimeNode = gameLog.SelectSingleNode("GameEndTime");
+                    XmlNode? gameRunningTimeNode = gameLog.SelectSingleNode("GameRunningTime");
+
                     GamePlayLogData gamePlayLogData = new()
                     {
-                        GameId = gameLog.SelectSingleNode("GameId").InnerText,
-                        GameName = gameLog.SelectSingleNode("GameName").InnerText,
-                        GameStartTime = gameLog.SelectSingleNode("GameStartTime").InnerText,
-                        GameEndTime = gameLog.SelectSingleNode("GameEndTime").InnerText,
-                        GameRunningTime = gameLog.SelectSingleNode("GameRunningTime").InnerText
+                        GameId = gameIdNode != null ? gameIdNode.InnerText : string.Empty,
+                        GameName = gameNameNode != null ? gameNameNode.InnerText : string.Empty,
+                        GameStartTime = gameStartTimeNode != null ? gameStartTimeNode.InnerText : string.Empty,
+                        GameEndTime = gameEndTimeNode != null ? gameEndTimeNode.InnerText : string.Empty,
+                        GameRunningTime = gameRunningTimeNode != null ? gameRunningTimeNode.InnerText : string.Empty
                     };
 
                     gamePlayLogDataCollection.Add(gamePlayLogData);
