4de2cc3eaf17ce382693e87e52190885dbdf4730
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-24 13:16:58 +0900

    DisableSpellCardDataColoring オプションの構成を追加

--- ThGameMgr.Ex/MainWindow.xaml.cs
+++ ThGameMgr.Ex/MainWindow.xaml.cs
@@ -1220,6 +1220,7 @@ namespace ThGameMgr.Ex
             }
 
             AutoBackupMenuItem.IsChecked = mainWindowSettings.AutoBackup;
+            DisableSpellCardDataColoringMenuItem.IsChecked = mainWindowSettings.DisableSpellCardDataColoring;
 
             SetGameSelectionMenu();
             SetPlayersFilterMenu();
