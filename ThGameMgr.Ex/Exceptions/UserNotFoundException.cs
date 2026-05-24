8dad8b807ade8a97cadb5738e6feec264be51dea
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 07:30:55 +0900

    名前の変更: InvalidUserException -> UserNotFoundException

---
+++ ThGameMgr.Ex/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace ThGameMgr.Ex.Exceptions
+{
+    class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base() { }
+        public UserNotFoundException(string message) : base(message) { }
+        public UserNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected UserNotFoundException(SerializationInfo info, StreamingContext context) { }
+    }
+}
