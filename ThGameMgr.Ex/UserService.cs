66c576cff95a79a4e5a50d6e403311dfccbb5579
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 07:22:13 +0900

    UserService 実装

---
+++ ThGameMgr.Ex/UserService.cs
@@ -0,0 +1,30 @@
+﻿namespace ThGameMgr.Ex
+{
+    internal class UserService : IUserService
+    {
+        public string CurrentUserName { get; set; }
+
+        public string CurrentUserDirectoryName { get; set; }
+
+        public UserService()
+        {
+            this.CurrentUserName = string.Empty;
+            this.CurrentUserDirectoryName = string.Empty;
+        }
+
+        public string GetCurrentUserSettingsDirectory()
+        {
+            return Path.Combine(PathInfo.AppLocation, this.CurrentUserDirectoryName, "Settings");
+        }
+
+        public string GetCurrentUserScoreBackupDirectoy()
+        {
+            return Path.Combine(PathInfo.AppLocation, this.CurrentUserDirectoryName, "backup");
+        }
+
+        public string GetCurrentUserGamePlayLogRecordFilePath()
+        {
+            return Path.Combine(PathInfo.AppLocation, this.CurrentUserDirectoryName, "GamePlayLog.xml");
+        }
+    }
+}
