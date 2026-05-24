b5b35fee51837028f646e8f58c5efdfcc214f986
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 07:34:21 +0900

    UserNotFoundException の実装を更新

--- ThGameMgr.Ex/Exceptions/UserNotFoundException.cs
+++ ThGameMgr.Ex/Exceptions/UserNotFoundException.cs
@@ -4,9 +4,25 @@ namespace ThGameMgr.Ex.Exceptions
 {
     class UserNotFoundException : Exception
     {
-        public UserNotFoundException() : base() { }
-        public UserNotFoundException(string message) : base(message) { }
-        public UserNotFoundException(string message, Exception inner) : base(message, inner) { }
-        protected UserNotFoundException(SerializationInfo info, StreamingContext context) { }
+        public string UserName { get; }
+        public UserNotFoundException(string userName) 
+            : base($"'{userName}' does not exist.") 
+        {
+            this.UserName = userName;
+        }
+
+        public UserNotFoundException(string userName, string message) : base(message) 
+        {
+            this.UserName = userName;
+        }
+        public UserNotFoundException(string userName, string message, Exception inner) : base(message, inner) 
+        { 
+            this.UserName = userName;
+        }
+
+        protected UserNotFoundException(string userName, SerializationInfo info, StreamingContext context) 
+        {
+            this.UserName = userName;
+        }
     }
 }
