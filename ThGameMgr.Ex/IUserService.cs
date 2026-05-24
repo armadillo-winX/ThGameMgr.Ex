c5226fc6280cfa1e989611cc35c209a35432df3c
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 07:11:15 +0900

    IUserService 追加

---
+++ ThGameMgr.Ex/IUserService.cs
@@ -0,0 +1,15 @@
+﻿namespace ThGameMgr.Ex
+{
+    internal interface IUserService
+    {
+        string CurrentUserName { get; set; }
+
+        string CurrentUserDirectoryName { get; set; }
+
+        string GetCurrentUserSettingsDirectory();
+
+        string GetCurrentUserScoreBackupDirectoy();
+
+        string GetCurrentUserGamePlayLogRecordFilePath();
+    }
+}
