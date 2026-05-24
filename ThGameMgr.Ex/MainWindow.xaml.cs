bde75e6130d1001a81acf402f5cfb295d517f4d1
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-24 13:40:30 +0900

    不要な分岐処理の削除

--- ThGameMgr.Ex/MainWindow.xaml.cs
+++ ThGameMgr.Ex/MainWindow.xaml.cs
@@ -2860,11 +2860,6 @@ namespace ThGameMgr.Ex
                                     (System.Windows.Media.Brush?)new System.Windows.Media.BrushConverter().ConvertFromString("#FE440A");
                             }
                         }
-                        else
-                        {
-                            e.Row.Background = System.Windows.Media.Brushes.White;
-                            e.Row.Foreground = System.Windows.Media.Brushes.Black;
-                        }
                     }
                 }
             }
