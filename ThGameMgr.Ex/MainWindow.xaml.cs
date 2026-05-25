b757ce890a9ffc2bfff35174b6f729db3cd392f7
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 15:54:24 +0900

    _currentUserService を readonly に変更

--- ThGameMgr.Ex/MainWindow.xaml.cs
+++ ThGameMgr.Ex/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace ThGameMgr.Ex
         private ScoreRecordDetailDialog? _scoreRecordDetailDialog = null;
         private SpellCardRecordDetailDialog? _spellCardRecordDetailDialog = null;
 
-        private IUserService _currentUserService;
+        private readonly IUserService _currentUserService;
 
         private enum DialogMode
         {
