6f2b8f3733178d022b6d8c527abac1c2ac86b871
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 17:03:43 +0900

    OnStartup 実装

--- ThGameMgr.Ex/App.xaml.cs
+++ ThGameMgr.Ex/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace ThGameMgr.Ex
 {
@@ -9,6 +7,26 @@ namespace ThGameMgr.Ex
     /// </summary>
     public partial class App : Application
     {
+        // DI コンテナプロバイダ
+        public static IServiceProvider ServiceProvider { get; private set; } = null!;
+
+        // OnStartup を override
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ServiceCollection services = new ServiceCollection();
+            // UserService をシングルトンとして追加
+            services.AddSingleton<IUserService, UserService>();
+            // MainWindow を DI コンテナに追加
+            services.AddTransient<MainWindow>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            // MainWindow のコンストラクタへ UserService を注入
+            MainWindow mainWindow = ServiceProvider.GetRequiredKeyedService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 
 }
