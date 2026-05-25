920f71248736db53c7c1b89544f0ae62fd3dfcec
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 16:45:09 +0900

    GetExternalTools メソッド修正

--- ThGameMgr.Ex/Dialogs/ManageExternalToolsDialog.xaml.cs
+++ ThGameMgr.Ex/Dialogs/ManageExternalToolsDialog.xaml.cs
@@ -46,7 +46,7 @@ namespace ThGameMgr.Ex.Dialogs
         {
             ExternalToolsListBox.Items.Clear();
 
-            string exToolsConfig = $"{UserConfigurator.CurrentUserDirectoryPath}\\Settings\\ExternalTools.xml";
+            string exToolsConfig = Path.Combine(this.SettingsDirectory, "ExternalTools.xml");
             if (File.Exists(exToolsConfig))
             {
                 XmlDocument exToolsConfigXml = new();
