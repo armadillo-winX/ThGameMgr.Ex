20b37888118f945c5080341661cd6e2571046c6d
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 18:00:35 +0900

    RestoreButtonClick 修正

--- ThGameMgr.Ex/Dialogs/ManageScoreBackupDialog.xaml.cs
+++ ThGameMgr.Ex/Dialogs/ManageScoreBackupDialog.xaml.cs
@@ -85,6 +85,7 @@ namespace ThGameMgr.Ex.Dialogs
                         MessageBoxButton.YesNo, MessageBoxImage.Information);
                         if (result == MessageBoxResult.Yes)
                         {
+                            string backupFilePath = Path.Combine(_currentUserService.GetCurrentUserScoreBackupDirectoy(), backupFile);
                             ScoreBackup.Restore(gameId, backupFile);
 
                             MessageBox.Show(this, "復元しました。", "スコアファイルの復元",
