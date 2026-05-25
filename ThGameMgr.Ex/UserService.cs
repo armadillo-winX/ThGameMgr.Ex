81de393f2cf57614049a8c10e98c06f8a5947a45
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-25 18:42:29 +0900

    summary コメント追加

--- ThGameMgr.Ex/UserService.cs
+++ ThGameMgr.Ex/UserService.cs
@@ -12,6 +12,12 @@
             this.CurrentUserDirectoryName = string.Empty;
         }
 
+        /// <summary>
+        /// アクティブなユーザーを切り替えます．
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="UserNotFoundException"></exception>
         public void SwitchUser(string userName)
         {
             if (string.IsNullOrWhiteSpace(userName))
@@ -24,21 +30,37 @@
             this.CurrentUserDirectoryName = userDirectoryName;
         }
 
+        /// <summary>
+        /// 現在アクティブなユーザーのユーザー名を取得します．
+        /// </summary>
+        /// <returns></returns>
         public string GetCurrentUserName()
         {
             return this.CurrentUserName;
         }
 
+        /// <summary>
+        /// 現在のユーザーの設定保存ディレクトリのパスを取得します．
+        /// </summary>
+        /// <returns></returns>
         public string GetCurrentUserSettingsDirectory()
         {
             return Path.Combine(PathInfo.UsersDirectory, this.CurrentUserDirectoryName, "Settings");
         }
 
+        /// <summary>
+        /// 現在のユーザーのスコアデータファイルのバックアップ格納ディレクトリのパスを取得します．
+        /// </summary>
+        /// <returns></returns>
         public string GetCurrentUserScoreBackupDirectoy()
         {
             return Path.Combine(PathInfo.UsersDirectory, this.CurrentUserDirectoryName, "backup");
         }
 
+        /// <summary>
+        /// 現在のユーザーのゲーム実行履歴ファイルを取得します．
+        /// </summary>
+        /// <returns></returns>
         public string GetCurrentUserGamePlayLogRecordFilePath()
         {
             return Path.Combine(PathInfo.UsersDirectory, this.CurrentUserDirectoryName, "GamePlayLog.xml");
