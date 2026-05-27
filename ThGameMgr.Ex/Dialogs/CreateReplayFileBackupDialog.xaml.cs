4265c429543eff8f3d9dd6613caf4f9ea93a822c
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-28 02:24:34 +0900

    CreateReplayFileBackupDialog 追加

---
+++ ThGameMgr.Ex/Dialogs/CreateReplayFileBackupDialog.xaml.cs
@@ -0,0 +1,70 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// CreateReplayFileBackupDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class CreateReplayFileBackupDialog : Window
+    {
+        private IUserService _currentUserService;
+
+        public string GameId { get; set; }
+
+        public string ReplayFilePath { get; set; }
+
+        public CreateReplayFileBackupDialog(
+            IUserService userService,
+            string gameId,
+            string replayFilePath)
+        {
+            InitializeComponent();
+
+            _currentUserService = userService;
+            this.GameId = gameId;
+            this.ReplayFilePath = replayFilePath;
+
+            string replayFileName = Path.GetFileName(replayFilePath);
+            NameBox.Text = $"{replayFileName} のバックアップ";
+        }
+
+        private void CreateBackupButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NameBox.Text))
+            {
+                string backupName = NameBox.Text;
+                string backupRootDirectory = _currentUserService.GetCurrentUserScoreBackupDirectoy();
+                string backupDirectory = Path.Combine(backupRootDirectory, this.GameId);
+                DateTime now = DateTime.Now;
+                ReplayFileBackupInfo replayFileBackupInfo = new()
+                {
+                    GameId = this.GameId,
+                    GameName = GameIndex.GetGameName(this.GameId),
+                    SourceReplayFilePath = this.ReplayFilePath,
+                    BackupName = backupName,
+                    Timestamp = now.ToString("yyyy/MM/dd hh:mm:s"),
+                    Comment = CommentBox.Text,
+                    ApplicationName = VersionInfo.AppName
+                };
+
+                try
+                {
+                    ReplayFileBackup.ReplayFileBackup.MakeReplayBackupFile(
+                        now.ToString("yyyy-MM-dd_hh-mm-ss"),
+                        replayFileBackupInfo,
+                        backupDirectory
+                        );
+                    this.DialogResult = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, $"バックアップの作成に失敗しました。\n{ex.Message}",
+                        "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show(this, "バックアップの名前は空にできません。", "リプレイファイルのバックアップ",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+    }
+}
