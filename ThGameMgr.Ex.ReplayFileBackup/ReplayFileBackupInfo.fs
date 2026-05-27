26f06df46347cbbe64568dbb0155a7a941f23a01
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-27 18:05:55 +0900

    ReplayFileBackupInfo 追加

---
+++ ThGameMgr.Ex.ReplayFileBackup/ReplayFileBackupInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace ThGameMgr.Ex.ReplayFileBackup
+
+open System.Xml.Serialization
+
+[<CLIMutable>]
+[<XmlRoot("ReplayFileBackupInfo")>]
+type ReplayFileBackupInfo = {
+    [<XmlElement("GameId")>] GameId: string
+    [<XmlElement("GameName")>] GameName: string
+    [<XmlElement("SourceReplayFilePath")>] SourceReplayFilePath: string
+    [<XmlElement("BackupName")>] BackupName: string
+    [<XmlElement("Timestamp")>] Timestamp: string
+    [<XmlElement("Comment")>] Comment: string
+    [<XmlElement("ApplicationName")>] ApplicationName: string
+}
