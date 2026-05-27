5e6eb31ff0a1bc8eb0cd44aaf5b3c505ce4abc30
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-28 00:41:32 +0900

    createTempDirectory を実装

--- ThGameMgr.Ex.ReplayFileBackup/IOUtil.fs
+++ ThGameMgr.Ex.ReplayFileBackup/IOUtil.fs
@@ -1,7 +1,15 @@
 ﻿namespace ThGameMgr.Ex.ReplayFileBackup
 
+open System
 open System.IO
+open System.Reflection
 
 module IOUtil =
-    
+    let private assemblyName = Assembly.GetExecutingAssembly().GetName().Name
 
+    let internal createTempDirectory () =
+        let tempDirectory = Path.GetTempPath()
+        let timestamp = DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss")
+        Path.Combine(tempDirectory, $"{assemblyName}_{timestamp}") 
+        |> Directory.CreateDirectory 
+        |> (fun d -> d.FullName)
