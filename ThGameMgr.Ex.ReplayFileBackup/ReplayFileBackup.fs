b5cbe46b0517d21d6e1c28165701a49fb0303f30
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-27 18:11:10 +0900

    makeReplayFileBackupInfoFile 実装

--- ThGameMgr.Ex.ReplayFileBackup/ReplayFileBackup.fs
+++ ThGameMgr.Ex.ReplayFileBackup/ReplayFileBackup.fs
@@ -1,5 +1,13 @@
 ﻿namespace ThGameMgr.Ex.ReplayFileBackup
 
+open System.IO
+open System.IO.Compression
+open System.Xml.Serialization
+
 module ReplayFileBackup =
-    let hello name =
-        printfn "Hello %s" name
+    let internal makeReplayFileBackupInfoFile (replayBackupInfo: ReplayFileBackupInfo) (baseDirectory: string) =
+        let serializer = XmlSerializer(typeof<ReplayFileBackupInfo>)
+        let replayBackupFilePath = Path.Combine(baseDirectory, "ReplayFileBackupInfo.xml")
+        let stream = new FileStream(replayBackupFilePath, FileMode.Create)
+        serializer.Serialize(stream, replayBackupInfo)
+        stream.Dispose()
