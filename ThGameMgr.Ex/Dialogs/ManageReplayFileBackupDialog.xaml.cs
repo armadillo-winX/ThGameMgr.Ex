49ac0244daa8ab1fcf6b0fe7398c9894a2381e11
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-28 03:14:51 +0900

    ManageReplayFileBackupDialog 追加

---
+++ ThGameMgr.Ex/Dialogs/ManageReplayFileBackupDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// ManageReplayFileBackupDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ManageReplayFileBackupDialog : Window
+    {
+        private readonly IUserService _currentUserService;
+
+        private string GameId { get; set; }
+
+        public ManageReplayFileBackupDialog(
+            IUserService userService,
+            string gameId
+            )
+        {
+            InitializeComponent();
+
+            _currentUserService = userService;
+            this.GameId = gameId;
+
+            DescriptionBlock.Text = $"{GameIndex.GetGameName(gameId)} のリプレイファイルバックアップ";
+
+            string replayBackupDirectory = Path.Combine(userService.GetCurrentUserReplayBackupDirectory(), gameId);
+            string[] backupFiles =
+                Directory.GetFiles(replayBackupDirectory, "*.trpb", SearchOption.TopDirectoryOnly);
+            foreach (string backupFile in backupFiles)
+            {
+                ReplayFileBackupInfo replayFileBackupInfo = 
+                    ReplayBackup.GetReplayBackupFileInfo(backupFile);
+                BackupFilesDataGrid.Items.Add(replayFileBackupInfo);
+            }
+        }
+    }
+}
