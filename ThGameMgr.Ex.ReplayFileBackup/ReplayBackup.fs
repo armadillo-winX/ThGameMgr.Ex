844b34ce8bd0c18810a1b0f422c296829d012a03
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-29 01:44:57 +0900

    ExtractBackupFile 修正

--- ThGameMgr.Ex.ReplayFileBackup/ReplayBackup.fs
+++ ThGameMgr.Ex.ReplayFileBackup/ReplayBackup.fs
@@ -51,15 +51,13 @@ module ReplayBackup =
         serializer.Deserialize(stream) 
         :?> ReplayFileBackupInfo
 
-    let ExtractBackupFile (replayBackupFilePath: string) (outputDirectory: string) =
+    let ExtractBackupFile (replayBackupFilePath: string) (outputFilePath: string) =
         let replayBackupInfo = GetReplayBackupFileInfo replayBackupFilePath
 
         let archive  = ZipFile.OpenRead(replayBackupFilePath)
         let replayFileEntry = 
             $"rpy/{Path.GetFileName(replayBackupInfo.SourceReplayFilePath)}" 
             |> archive.GetEntry
-        let outputFilePath = 
-            Path.Combine(outputDirectory, Path.GetFileName(replayBackupInfo.SourceReplayFilePath))
 
         replayFileEntry.ExtractToFile(outputFilePath, true)
 
