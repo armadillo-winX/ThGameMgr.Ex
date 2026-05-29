7fee88f83cad7d6ed17be21470157093c9b1da8b
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-29 13:36:18 +0900

    GameId モジュール実装

--- ThGameMgr.Ex.Core/GameId.fs
+++ ThGameMgr.Ex.Core/GameId.fs
@@ -1,5 +1,61 @@
 ﻿namespace ThGameMgr.Ex.Core
 
+[<RequireQualifiedAccess>]
 module GameId =
-    let hello name =
-        printfn "Hello %s" name
+    
+    [<Literal>]
+    let Th01 = "Th01"
+
+    [<Literal>]
+    let Th02 = "Th02"
+
+    [<Literal>]
+    let Th03 = "Th03"
+
+    [<Literal>]
+    let Th04 = "Th04"
+
+    [<Literal>]
+    let Th05 = "Th05"
+
+    [<Literal>]
+    let Th06 = "Th06"
+
+    [<Literal>]
+    let Th07 = "Th07"
+
+    [<Literal>]
+    let Th08 = "Th08"
+
+    [<Literal>]
+    let Th09 = "Th09"
+
+    [<Literal>]
+    let Th10 = "Th10"
+
+    [<Literal>]
+    let Th11 = "Th11"
+
+    [<Literal>]
+    let Th12 = "Th12"
+
+    [<Literal>]
+    let Th13 = "Th13"
+
+    [<Literal>]
+    let Th14 = "Th14"
+
+    [<Literal>]
+    let Th15 = "Th15"
+
+    [<Literal>]
+    let Th16  ="Th16"
+
+    [<Literal>]
+    let Th17 = "Th17"
+
+    [<Literal>]
+    let Th18 = "Th18"
+
+    [<Literal>]
+    let Th19 = "Th19"
