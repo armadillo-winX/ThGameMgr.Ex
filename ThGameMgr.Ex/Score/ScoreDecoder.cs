e9da11b396b542cd91eac8703a90e5e16409916d
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-29 17:24:24 +0900

    プロパティの呼び出しの修正

--- ThGameMgr.Ex/Score/ScoreDecoder.cs
+++ ThGameMgr.Ex/Score/ScoreDecoder.cs
@@ -4,55 +4,55 @@
     {
         public static bool Decode(string gameId, string scorePath, Stream outputData)
         {
-            if (gameId == GameIndex.Th06)
+            if (gameId == GameIdIndex.Th06)
             {
                 return Th06.Th06Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th07)
+            else if (gameId == GameIdIndex.Th07)
             {
                 return Th07.Th07Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th08)
+            else if (gameId == GameIdIndex.Th08)
             {
                 return Th08.Th08Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th09)
+            else if (gameId == GameIdIndex.Th09)
             {
                 return Th09.Th09Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th10)
+            else if (gameId == GameIdIndex.Th10)
             {
                 return Th10.Th10Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th11)
+            else if (gameId == GameIdIndex.Th11)
             {
                 return Th11.Th11Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th12)
+            else if (gameId == GameIdIndex.Th12)
             {
                 return Th12.Th12Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th13)
+            else if (gameId == GameIdIndex.Th13)
             {
                 return Th13.Th13Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th14)
+            else if (gameId == GameIdIndex.Th14)
             {
                 return Th14.Th14Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th15)
+            else if (gameId == GameIdIndex.Th15)
             {
                 return Th15.Th15Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th16)
+            else if (gameId == GameIdIndex.Th16)
             {
                 return Th16.Th16Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th17)
+            else if (gameId == GameIdIndex.Th17)
             {
                 return Th17.Th17Decoder.Convert(scorePath, outputData);
             }
-            else if (gameId == GameIndex.Th18)
+            else if (gameId == GameIdIndex.Th18)
             {
                 return Th18.Th18Decoder.Convert(scorePath, outputData);
             }
