4c54b5dd2e30b045fb0f5a1cb40b4df743627651
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-05-30 00:38:49 +0900

    startGameProcessExecute/StartGameProcess を実装

--- ThGameMgr.Ex.GameProcess/Library.fs
+++ ThGameMgr.Ex.GameProcess/Library.fs
@@ -1,5 +1,33 @@
 ﻿namespace ThGameMgr.Ex.GameProcess
 
+open System
+open System.Diagnostics
+open System.IO
+open ThGameMgr.Ex.Core
+
 module GameProcessManager =
-    let hello name =
-        printfn "Hello %s" name
+
+    let internal startGameProcessExecute (gameId: string) (gameExecutableFilePath: string) =
+        if File.Exists(gameExecutableFilePath) = true then
+            let gameDirectory : string option = Path.GetDirectoryName(gameExecutableFilePath) |> Option.ofObj
+            match gameDirectory with
+                | Some dir ->
+                    let processStartInfo = ProcessStartInfo()
+                    processStartInfo.FileName <- gameExecutableFilePath
+                    processStartInfo.WorkingDirectory <- dir
+                    processStartInfo.UseShellExecute <- true
+                    Process.Start(processStartInfo)
+                |None -> 
+                    raise (InvalidOperationException($"Cannot find the installation directory of {GameNameIndex.GetGameNameFromId(gameId)}"))
+        else
+            raise (FileNotFoundException(
+            $"The executable file of {GameNameIndex.GetGameNameFromId(gameId)} does not found."))
+
+    let StartGameProcess (gameId: string) (gameExecutableFilePath: string) =
+        let gameIdOption = Option.ofObj gameId
+        let gameExecutableFilePathOption = Option.ofObj gameExecutableFilePath
+
+        match (gameIdOption, gameExecutableFilePathOption) with
+            | (Some id, Some path) -> startGameProcessExecute id path
+            | (None, _) -> nullArg "gameId"
+            | (_, None) -> nullArg "gameExecutableFilePath"
