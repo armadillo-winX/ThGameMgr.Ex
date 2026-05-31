4f79eab935f85afcee3f120fe7a69c5ffd055212
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-01 00:45:23 +0900

    GameWindowManager 実装

--- ThGameMgr.Ex.GameProcess/GameWindowManager.fs
+++ ThGameMgr.Ex.GameProcess/GameWindowManager.fs
@@ -1,8 +1,31 @@
 namespace ThGameMgr.Ex.GameProcess
 
 open System
+open ThGameMgr.Ex.Win32API
 
 module GameWindowManager =
-    let ResizeWindow () =
-        // ここにコードを書く
-        0
+
+    let GetGameWindowSizes (gameWindow: IntPtr) =
+        let result, rect = User32.GetWindowRect(gameWindow)
+        let width = rect.right - rect.left
+        let height = rect.top - rect.bottom
+
+        let gameWindowSizes: GameWindowSizes = {
+            Width = width
+            Height = height
+        }
+        gameWindowSizes
+
+    let GetGameWindowPosition (gameWindow: IntPtr) =
+        let result, rect = User32.GetWindowRect(gameWindow)
+
+        let gameWindowPosition: GameWindowPosition = {
+            X = rect.left
+            Y = rect.top
+        }
+        gameWindowPosition
+
+    let ResizeGameWindow (gameWindow: IntPtr) (width: int) (height: int) =
+        let position = GetGameWindowPosition gameWindow
+        User32.MoveWindow(gameWindow, position.X, position.Y, width, height, 1) |> ignore
+        User32.SetForegroundWindow(gameWindow) |> ignore
