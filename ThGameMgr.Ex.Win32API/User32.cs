0cbda2a962560dd7e0d1dc68dd2c8f2e86802266
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-01 00:26:11 +0900

    User32 実装

--- ThGameMgr.Ex.Win32API/User32.cs
+++ ThGameMgr.Ex.Win32API/User32.cs
@@ -1,7 +1,34 @@
 ﻿namespace ThGameMgr.Ex.Win32API
 {
-    public class User32
+    public partial class User32
     {
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
 
+        [LibraryImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool GetWindowRect(IntPtr hWnd, out RECT rect);
+
+        [LibraryImport("user32.dll", SetLastError = true)]
+        private static partial int MoveWindow(IntPtr hwnd, int x, int y,
+            int nWidth, int nHeight, int bRepaint);
+
+        [LibraryImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+
+        [LibraryImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool SetForegroundWindow(IntPtr hWnd);
+
+        [LibraryImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool IsWindow(IntPtr hWnd);
     }
 }
