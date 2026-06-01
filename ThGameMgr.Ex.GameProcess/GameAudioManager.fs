30f4a777c827abc0c077c8cf7b5494312070c02f
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-01 18:42:41 +0900

    setGameAudioVolumeFromCollection 実装

--- ThGameMgr.Ex.GameProcess/GameAudioManager.fs
+++ ThGameMgr.Ex.GameProcess/GameAudioManager.fs
@@ -28,19 +28,23 @@ module GameAudioManager =
             | Some p -> getGameAudioVolumeExecute p
             | None -> nullArg "gameProcess"
 
+    let rec private setGameAudioVolumeFromCollection (gameProcess: Process) (volume: float) (index: int) (sessionCollection: SessionCollection) =
+        if index < sessionCollection.Count then
+            let session = sessionCollection[index]
+            match session with
+            | s when int s.GetProcessID = gameProcess.Id ->
+                s.SimpleAudioVolume.Volume <- float32 volume
+                true
+            |_-> setGameAudioVolumeFromCollection gameProcess volume (index+1) sessionCollection
+        else
+            false
+
     let private setGameAudioVolumeExecute (gameProcess: Process) (volume: float) =
         let enumerator = new MMDeviceEnumerator()
         let defaultAudioDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console)
         let audioSessionManager = defaultAudioDevice.AudioSessionManager
         let sessionCollection = audioSessionManager.Sessions
-        
-        let mutable i = 0
-        while i < sessionCollection.Count do
-            let session = sessionCollection[i]
-            if int session.GetProcessID = gameProcess.Id then
-                session.SimpleAudioVolume.Volume <- float32 volume
-
-            i <- i + 1
+        setGameAudioVolumeFromCollection gameProcess volume 0 sessionCollection
 
     let SetGameAudioVolume (gameProcess: Process) (volume: float) =
         let gameProcessOption = gameProcess |> Option.ofObj
