f9a0b87e0f131d62ee15ba73d7a6c0556fa9370e
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-01 17:29:43 +0900

    getGameAudioVolumeExecute/getGameAudioVolumeFromCollection 実装

--- ThGameMgr.Ex.GameProcess/GameAudioManager.fs
+++ ThGameMgr.Ex.GameProcess/GameAudioManager.fs
@@ -5,3 +5,19 @@ open System.Diagnostics
 
 module GameAudioManager =
     
+    let rec private getGameAudioVolumeFromCollection (gameProcess: Process) (index: int) (sessionCollection: SessionCollection) =
+        if index < sessionCollection.Count then
+            let session = sessionCollection[index]
+            match session with
+            | s when int s.GetProcessID = gameProcess.Id ->
+                float s.SimpleAudioVolume.Volume
+            |_-> getGameAudioVolumeFromCollection gameProcess (index+1) sessionCollection
+        else
+            0.0
+
+    let private getGameAudioVolumeExecute (gameProcess: Process) =
+        let enumerator = new MMDeviceEnumerator()
+        let defaultAudioDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console)
+        let audioSessionManager = defaultAudioDevice.AudioSessionManager
+        let sessionCollection = audioSessionManager.Sessions
+        getGameAudioVolumeFromCollection gameProcess 0 sessionCollection
