20b0e7da0f49295ccc6b5ef2527aa0323c826322
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-28 19:36:06 +0900

    FunctionEnvironmentBuilder() を実装

--- ThGameMgr.Ex.Masicalan/ExtensionBulder.fs
+++ ThGameMgr.Ex.Masicalan/ExtensionBulder.fs
@@ -2,6 +2,7 @@
 
 open Masicalan.Core
 open System
+open System.Collections.Generic
 
 module ExtensionBulder =
     let rec private toMasicalanVal (o: obj) : Value =
@@ -31,3 +32,25 @@ module ExtensionBulder =
 
             // デリゲートを動的に呼び出し
             deleg.DynamicInvoke(csArgs) |> toMasicalanVal
+
+    type FunctionEnvironmentBuilder() =
+        let env = Map.empty<string, string list * Statement>
+
+        /// <summary>
+        /// Extension 関数環境に対して関数を登録します．
+        /// name: 関数の名前
+        /// paramName: 引数名リスト
+        /// deleg: 登録する関数
+        /// </summary>
+        member this.Register (name: string) (paramNames: string list) (deleg: Delegate) =
+            let nativeFunction = transferCsDelegate deleg
+            let dummyArgs = paramNames |> List.map Expression.Var
+            let statement = Statement.CallNativeF (nativeFunction, dummyArgs)
+
+            env.Add(name, (paramNames, statement))
+
+        /// <summary>
+        /// 構築された関数環境を取得します．
+        /// </summary>
+        member this.Build() =
+            env
