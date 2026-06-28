76e8fb79fa2d21c82df2427f97042abbe6b27a4f
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-29 00:09:13 +0900

    global using ThGameMgr.Ex.Masicalan 追加

--- ThGameMgr.Ex/GlobalUsing.cs
+++ ThGameMgr.Ex/GlobalUsing.cs
@@ -11,6 +11,7 @@ global using ThGameMgr.Ex.Exceptions;
 global using ThGameMgr.Ex.Extensions;
 global using ThGameMgr.Ex.Game;
 global using ThGameMgr.Ex.GameProcess;
+global using ThGameMgr.Ex.Masicalan;
 global using ThGameMgr.Ex.Plugin;
 global using ThGameMgr.Ex.Replay;
 global using ThGameMgr.Ex.ReplayFileBackup;
