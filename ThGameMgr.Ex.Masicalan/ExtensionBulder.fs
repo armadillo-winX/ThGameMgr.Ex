3f72ce86edfd9c9dc13cd8ee3c408ad1e5c25230
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-06-28 18:09:25 +0900

    toMasicalanVal/toCsObj 実装

--- ThGameMgr.Ex.Masicalan/ExtensionBulder.fs
+++ ThGameMgr.Ex.Masicalan/ExtensionBulder.fs
@@ -1,5 +1,24 @@
 ﻿namespace ThGameMgr.Ex.Masicalan
 
+open Masicalan.Core
+open System
+
 module ExtensionBulder =
-    let hello name =
-        printfn "Hello %s" name
+    let rec private toMasicalanVal (o: obj) : Value =
+        match o with
+        | :? int as i -> Value.IntVal i
+        | :? float as f -> Value.FloatVal f
+        | :? string as s -> Value.StringVal s
+        | :? bool as b -> Value.BoolVal b
+        | ob when ob.GetType() = typeof<Void> -> Value.VoidVal
+        | :? list<obj> as l -> Value.ArrayVal (List.map toMasicalanVal l)
+        |_ -> failwithf $"Unsupported type: {o.GetType().Name}"
+
+    let rec private toCsObject (v: Value): obj =
+        match v with
+        | Value.IntVal i -> i
+        | Value.FloatVal f -> f
+        | Value.StringVal s -> s
+        | Value.BoolVal b -> b
+        | Value.VoidVal -> null
+        | Value.ArrayVal a -> List.map toCsObject a
