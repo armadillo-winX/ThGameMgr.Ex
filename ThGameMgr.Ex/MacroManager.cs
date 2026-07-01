fc963725da054eb12d5138c211d92985ef871479
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-01 21:54:04 +0900

    MacroManager クラス追加

---
+++ ThGameMgr.Ex/MacroManager.cs
@@ -0,0 +1,54 @@
+﻿using Masicalan.VaultVfs;
+using System.Security.Cryptography;
+
+namespace ThGameMgr.Ex
+{
+    internal class MacroManager
+    {
+        private IUserService _userService;
+        public MacroManager(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        internal string GenerateVfsEntropyName()
+        {
+            string guid = Guid.NewGuid().ToString("N");
+            string entropyName = $"ThGameMgr.Ex.{guid}";
+
+            return entropyName;
+        }
+
+        internal void SaveVfsEntropy(string entropyName)
+        {
+            string configFilePath = _userService.GetCurrentUserMacroArchiveEntropyConfigPath();
+            byte[] entropy = Encoding.UTF8.GetBytes(entropyName);
+            byte[] encrypted = ProtectedData.Protect(entropy, null, DataProtectionScope.CurrentUser);
+            File.WriteAllBytes(configFilePath, encrypted);
+        }
+
+        internal string GetVfsEntropyName()
+        {
+            string configFilePath = _userService.GetCurrentUserMacroArchiveEntropyConfigPath();
+            if (File.Exists(configFilePath))
+            {
+                byte[] encrypted = File.ReadAllBytes(configFilePath);
+                byte[] entropy = ProtectedData.Unprotect(encrypted, null, DataProtectionScope.CurrentUser);
+                return Encoding.UTF8.GetString(entropy);
+            }
+            else
+            {
+                string entropyName = GenerateVfsEntropyName();
+                SaveVfsEntropy(entropyName);
+                return entropyName;
+            }
+        }
+
+        internal string CreateVfs()
+        {
+            string vfsArchiveFilePath = _userService.GetCurrentUserMacroVaultArchiveFilePath();
+            string entropyName = GetVfsEntropyName();
+            return VfsManager.Create(vfsArchiveFilePath, entropyName);
+        }
+    }
+}
