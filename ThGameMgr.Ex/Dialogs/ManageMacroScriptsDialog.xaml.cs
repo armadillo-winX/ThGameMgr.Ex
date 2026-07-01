fe5e7606c279ad12601ecb2ceda2bd2328e6d968
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-01 20:54:27 +0900

    ManageMacroScriptsDialog 追加

---
+++ ThGameMgr.Ex/Dialogs/ManageMacroScriptsDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// ManageMacroScriptsDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ManageMacroScriptsDialog : Window
+    {
+        public ManageMacroScriptsDialog(IUserService userService)
+        {
+            InitializeComponent();
+        }
+
+        private string GenerateVfsEntropyName(string userDirName)
+        {
+            string guid = Guid.NewGuid().ToString("N");
+            string entropyName = $"ThGameMgr.Ex.{userDirName}.{guid}";
+
+            return entropyName;
+        }
+    }
+}
