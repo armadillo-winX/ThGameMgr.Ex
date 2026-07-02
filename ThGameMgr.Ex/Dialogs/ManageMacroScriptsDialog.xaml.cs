7829215a7790e941a4895fc1656ee417305bb907
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-03 01:43:00 +0900

    ManageMacroScriptDialog: 初期化処理の追加

--- ThGameMgr.Ex/Dialogs/ManageMacroScriptsDialog.xaml.cs
+++ ThGameMgr.Ex/Dialogs/ManageMacroScriptsDialog.xaml.cs
@@ -8,6 +8,20 @@
         public ManageMacroScriptsDialog(IUserService userService)
         {
             InitializeComponent();
+
+            string vfsFilePath = userService.GetCurrentUserMacroVaultArchiveFilePath();
+            if (!File.Exists(vfsFilePath))
+            {
+                try
+                {
+                    MacroManager macroManager = new(userService);
+                    macroManager.CreateVfs();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Failed to create vfs archive: {ex.Message}");
+                }
+            }
         }
     }
 }
