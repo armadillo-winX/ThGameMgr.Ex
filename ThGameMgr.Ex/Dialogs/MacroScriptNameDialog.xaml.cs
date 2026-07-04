d1b9f10b8d8dbe2eb0d1ebf96189cbb4b6c9d48f
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-04 12:11:29 +0900

    MacroScriptNameDialog 追加実装

---
+++ ThGameMgr.Ex/Dialogs/MacroScriptNameDialog.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// MacroScriptNameDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MacroScriptNameDialog : Window
+    {
+        public string ScriptName { get; set; }
+
+        public MacroScriptNameDialog()
+        {
+            InitializeComponent();
+            _ = NameBox.Focus();
+
+            this.ScriptName = string.Empty;
+        }
+
+        private void OKButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.ScriptName = NameBox.Text + ".masis";
+            this.DialogResult = true;
+        }
+    }
+}
