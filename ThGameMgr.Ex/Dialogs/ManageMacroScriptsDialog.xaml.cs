f8402eaf45b263149aa8dff8759b9342389c5716
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-04 12:26:46 +0900

    CreateButtonClick 追加

--- ThGameMgr.Ex/Dialogs/ManageMacroScriptsDialog.xaml.cs
+++ ThGameMgr.Ex/Dialogs/ManageMacroScriptsDialog.xaml.cs
@@ -5,9 +5,13 @@
     /// </summary>
     public partial class ManageMacroScriptsDialog : Window
     {
+        private readonly IUserService _userService;
+
         public ManageMacroScriptsDialog(IUserService userService)
         {
             InitializeComponent();
+
+            _userService = userService;
 
             string vfsFilePath = userService.GetCurrentUserMacroVaultArchiveFilePath();
             if (!File.Exists(vfsFilePath))
@@ -20,6 +24,39 @@
                 catch (Exception ex)
                 {
                     Debug.WriteLine($"Failed to create vfs archive: {ex.Message}");
+                }
+            }
+        }
+
+        private void CreateButtonClick(object sender, RoutedEventArgs e)
+        {
+            MacroScriptNameDialog macroScriptNameDialog = new()
+            {
+                Owner = this
+            };
+
+            if (macroScriptNameDialog.ShowDialog() == true)
+            {
+                string scriptName = macroScriptNameDialog.ScriptName;
+                try
+                {
+                    MacroManager macroManager = new(_userService);
+                    macroManager.AddScript("", "", scriptName);
+
+                    MacroScriptEditorDialog macroScriptEditorDialog = new(
+                        _userService, scriptName
+                        )
+                    {
+                        Owner = this
+                    };
+
+                    macroScriptEditorDialog.Show();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        this, $"スクリプトファイルを追加できませんでした．\n{ex.Message}", "エラー",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
