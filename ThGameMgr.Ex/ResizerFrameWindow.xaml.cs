c7111444a1d0e5ad2df6dcaab42de4f5f00a31a4
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-19 23:23:04 +0900

    WidthBox/HeightBox のエンターキーイベントを実装

--- ThGameMgr.Ex/ResizerFrameWindow.xaml.cs
+++ ThGameMgr.Ex/ResizerFrameWindow.xaml.cs
@@ -172,5 +172,95 @@ namespace ThGameMgr.Ex
         {
             this.Close();
         }
+
+        private void WidthBoxKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                float width;
+                float height;
+
+                bool result0 = float.TryParse(WidthBox.Text, out width);
+                bool result1 = float.TryParse(HeightBox.Text, out height);
+
+                if (!result0 || !result1)
+                {
+                    MessageBox.Show(
+                        this, "入力が正しくありません．", "エラー",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    WidthBox.Text = (this.Width - 36).ToString();
+                    HeightBox.Text = (this.Height - 36).ToString();
+
+                    return;
+                }
+
+                if (FixAspectRateCheckBox.IsChecked == true)
+                {
+                    this.Width = width + 36;
+                }
+                else
+                {
+                    this.Width = width + 36;
+                    this.Height = height + 36;
+                }
+
+                try
+                {
+                    int iwidth = (int)(this.Width - 36);
+                    int iheight = (int)(this.Height - 36);
+
+                    GameWindowManager.ResizeGameWindow(this.GameWindow, iwidth, iheight);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void HeightBoxKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                float width;
+                float height;
+
+                bool result0 = float.TryParse(WidthBox.Text, out width);
+                bool result1 = float.TryParse(HeightBox.Text, out height);
+
+                if (!result0 || !result1)
+                {
+                    MessageBox.Show(
+                        this, "入力が正しくありません．", "エラー",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    WidthBox.Text = (this.Width - 36).ToString();
+                    HeightBox.Text = (this.Height - 36).ToString();
+
+                    return;
+                }
+
+                if (FixAspectRateCheckBox.IsChecked == true)
+                {
+                    this.Height = height + 36;
+                }
+                else
+                {
+                    this.Width = width + 36;
+                    this.Height = height + 36;
+                }
+
+                try
+                {
+                    int iwidth = (int)(this.Width - 36);
+                    int iheight = (int)(this.Height - 36);
+
+                    GameWindowManager.ResizeGameWindow(this.GameWindow, iwidth, iheight);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
     }
 }
