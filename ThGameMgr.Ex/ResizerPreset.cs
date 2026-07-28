0e1b7c24bfd4c45ffabb2a5e352be62a092ec686
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-07-28 16:20:33 +0900

    ResizerPreset 追加

---
+++ ThGameMgr.Ex/ResizerPreset.cs
@@ -0,0 +1,11 @@
+﻿namespace ThGameMgr.Ex
+{
+    public class ResizerPreset
+    {
+        public int ResizeWidth { get; set; }
+
+        public int ResizeHeight { get; set; }
+
+        public bool FixAspectRate { get; set; }
+    }
+}
