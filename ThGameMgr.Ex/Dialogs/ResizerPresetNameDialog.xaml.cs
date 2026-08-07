5ca9e8dd40c4153ed71d2f5488be1919f535f0da
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-08-08 01:42:37 +0900

    ResizerPresetNameDialog 追加

---
+++ ThGameMgr.Ex/Dialogs/ResizerPresetNameDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace ThGameMgr.Ex.Dialogs
+{
+    /// <summary>
+    /// ResizerPresetNameDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ResizerPresetNameDialog : Window
+    {
+        public string PresetName { get; set; }
+
+        public ResizerPresetNameDialog(string? defaultName)
+        {
+            InitializeComponent();
+
+            NameBox.Text = defaultName;
+            _ = NameBox.Focus();
+
+            this.PresetName = defaultName != null ? defaultName : string.Empty;
+        }
+
+        private void OKButtonClick(object sender, RoutedEventArgs e)
+        {
+            string name = NameBox.Text;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                this.PresetName = name;
+                this.DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show(this,
+                    "プリセット名を入力してください．", "リサイザプリセットを保存",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+    }
+}
