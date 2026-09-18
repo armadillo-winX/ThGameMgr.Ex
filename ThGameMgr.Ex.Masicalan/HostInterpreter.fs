c649363531287a26c0f9150c2360ea4598fe5482
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2026-09-19 03:58:56 +0900

    Run 関数の実装

--- ThGameMgr.Ex.Masicalan/HostInterpreter.fs
+++ ThGameMgr.Ex.Masicalan/HostInterpreter.fs
@@ -1,5 +1,18 @@
 ﻿namespace ThGameMgr.Ex.Masicalan
 
+open Masicalan.Core
+
 module HostInterpreter =
-    let hello name =
-        printfn "Hello %s" name
+
+    /// <summary>
+    /// Masicalan スクリプトを実行します．
+    /// </summary>
+    /// <param name="script">スクリプト</param>
+    /// <param name="extVarEnv">Extension 変数環境</param>
+    /// <param name="extFunEnv">Extension 関数環境</param>
+    let Run (script: string) (extVarEnv: Map<string, (Value * Mutability)>) (extFunEnv: Map<string, (string list * Statement)>) =
+        try
+            Interpreter.RunWithExt script extVarEnv extFunEnv
+        with
+        | :? System.Reflection.TargetInvocationException as ex -> raise ex.InnerException
+        |_ as ex -> raise ex
